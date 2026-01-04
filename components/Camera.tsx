4ed5ee3bc636992ade0d53a02aa69d9c624da116
phuwadon <ppuwdon@gmail.com>
2026-01-04 18:42:48 +0700

    update camera button

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -4,6 +4,7 @@ import { useZxing } from "react-zxing";
 
 export default function Camera({studentcheck,setstudentcheck,checksuccess,setchecksuccess,setissave}:any) {
     const [device,setuseState] = useState<string>("");
+    const [camera,setCarmera] = useState<number>(0)
 
     useEffect(() => {
         (async () => {
@@ -11,15 +12,22 @@ export default function Camera({studentcheck,setstudentcheck,checksuccess,setche
           const videoDevices = allDevices.filter(d => d.kind === "videoinput");
         
           if (videoDevices.length > 0) {
-            setuseState(videoDevices[2].deviceId);
+            const getcameranumber = localStorage.getItem("camera");
+            if (getcameranumber) {
+                setuseState(videoDevices[+getcameranumber].deviceId);
+            }
+            else {
+                setuseState(videoDevices[camera].deviceId);
+            }
+            
           }
         })();
-    },[]);
+    },[camera]);
 
     //!handle camera
 
     const { ref } = useZxing({
-        deviceId: device,
+        deviceId: device !== "" ? device : "NOCAMERA",
         onDecodeResult(result) {
             let success:any = {};
             const findstudent = studentcheck.map((e:any) => {
@@ -61,8 +69,15 @@ export default function Camera({studentcheck,setstudentcheck,checksuccess,setche
 
     return(
         <div className="fixed mt-[10px] h-[calc(100dvh_-_50px)] w-full bg-black/80 flex gap-[20px] z-[100]">
-            <video ref={ref} autoPlay className="w-[600px] h-[455px] border-2 border-gray-400 rounded-lg shadow-md"/>
-            <div className="overflow-y-scroll w-[400px] grow-1">
+            <div>
+                <video ref={ref} autoPlay className="w-[600px] h-[455px] border-2 border-gray-400 rounded-lg shadow-md"/>
+                <div className="flex mt-[20px]">
+                    <div onClick={() => {setCarmera(0); localStorage.setItem("camera","0")}} className="bg-white text-black mr-[20px] p-[2px_20px] rounded-2xl cursor-pointer">กล้อง 0</div>
+                    <div onClick={() => {setCarmera(1); localStorage.setItem("camera","1")}} className="bg-white text-black mr-[20px] p-[2px_20px] rounded-2xl cursor-pointer">กล้อง 1</div>
+                    <div onClick={() => {setCarmera(2); localStorage.setItem("camera","2")}} className="bg-white text-black mr-[20px] p-[2px_20px] rounded-2xl cursor-pointer">กล้อง 2</div>
+                </div>
+            </div>
+            <div className="overflow-y-scroll w-[400px] p-[20px_0] grow-1">
                 {checksuccess.map((e:any,i:number) => (
                     <p key={i}>{checksuccess.length - 1 - i + 1}. {e.name} {e.lastname}</p>
                 ))}
--- components/VersionDisplay.tsx
+++ components/VersionDisplay.tsx
@@ -1,7 +1,7 @@
 export default function VersionDisplay() {
     return(
         <div className="text-white fixed bottom-[5px] left-[5px]">
-            <p>1.0.2</p>
+            <p>1.0.3</p>
         </div>
     )
 }
